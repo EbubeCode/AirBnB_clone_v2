a4a5ceb01b206f1976fce26a53fce3042784e2f7
EbubeCode <chukwuma258@gmail.com>
2022-09-14 03:42:18 +0100

    v2 task 6

--- console.py
+++ console.py
@@ -137,7 +137,7 @@ class HBNBCommand(cmd.Cmd):
                     new_instance.__dict__[k] = int(v)
             except Exception:
                 pass
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
         storage.save()
 
@@ -221,11 +221,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(HBNBCommand.classes[args]).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+import os
 
 
-storage = FileStorage()
+s = os.getenv('HBNB_TYPE_STORAGE')
+storage = None
+if s == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
