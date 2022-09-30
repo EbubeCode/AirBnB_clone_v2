2454d684c0a6d53e46b79c71ec115253d743e267
EbubeCode <chukwuma258@gmail.com>
2022-09-30 03:02:43 +0100

    prepare web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 # setup script for webstatic deployment
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-printf "<html>\n<head></head>\n<body>Hello World!</body><html>\n" > /data/web_static/releases/test/index.html
+sudo printf "<html>\n<head></head>\n<body>Hello World!</body><html>\n" > /data/web_static/releases/test/index.html
 if [ -e /data/web_static/current ]; then
-	rm /data/web_static/current
+	sudo rm /data/web_static/current
 fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo truncate -s 0 /etc/nginx/sites-available/default
 printf "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\
 	\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\
 	\n\terror_page 404 403 500 503 /error-page.html;\n\tadd_header X-Served-By %s;\
 	\n\tserver_name _;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\tlocation = /error-page.html {\
 	\n\t\tinternal;\n\t}\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n}\n" \
-	"$HOSTNAME"> /etc/nginx/sites-available/default
-service nginx restart
+	"$HOSTNAME" | sudo tee -a /etc/nginx/sites-available/default >> /dev/null
+sudo service nginx restart
