a72ace934be397afc835ff1d3e95209765b1cdee
EbubeCode <chukwuma258@gmail.com>
2022-10-16 02:46:21 +0100

    flask task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Flask application '''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    ''' root endpoint for flask app'''
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' path hbnb which returns a string'''
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
