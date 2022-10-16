cdc3bb5d8077a2c30d26200d039075d6e9cad6b9
EbubeCode <chukwuma258@gmail.com>
2022-10-16 11:32:31 +0100

    flask task 7

--- models/state.py
+++ models/state.py
@@ -3,6 +3,10 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
+
+
+s = os.getenv('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
@@ -11,12 +15,12 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='delete')
 
-    @property
-    def cities(self):
-        """getter attribute to show FileStorge relationship
-        between city and state"""
-        from models import storage
-        if type(storage).__name__ == 'FileStorage':
+    if s != 'db':
+        @property
+        def cities(self):
+            """getter attribute to show FileStorge relationship
+            between city and state"""
+            from models import storage
             from models.city import City
             cities = storage.all(City)
             state_city = []
