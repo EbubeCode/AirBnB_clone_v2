ee40e940d54ea26e5ceb69d68eb816c1ddb046ed
EbubeCode <chukwuma258@gmail.com>
2022-10-16 11:40:35 +0100

    flask task 7

--- models/state.py
+++ models/state.py
@@ -11,9 +11,12 @@ s = os.getenv('HBNB_TYPE_STORAGE')
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='delete')
+    if s == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ""
 
     if s != 'db':
         @property
